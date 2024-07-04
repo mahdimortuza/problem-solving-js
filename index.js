ff49b16f097a0cf9bd25851319e68724a945b891
mahdimortuza <mahdi.webx@gmail.com>
2024-07-04 22:40:27 +0600

    problem 2 solved

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 // problem-1: Write a function that takes a string as input and returns the string reversed.
 
-function reversedString(str){
+{
+    function reversedString(str){
     return str.split('').reverse().join()
 }
 
 const result = reversedString("Hello world")
-console.log(result);
+// console.log(result);
+}
+
+// problem-2: Write a function that returns the largest number from an array of numbers.
+{
+function findLargestNumber(arr){
+    return Math.max(...arr)
+}
+const result = findLargestNumber([5, 150, 8, 10, 87])
+// console.log(result);
+}
